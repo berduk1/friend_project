a10ef5d503ed0ab30d2349148b5a38573d3c44f6
berduk1 <bernard.dukic@gmail.com>
2024-08-27 20:42:39 +0200

    Table

--- app.js
+++ app.js
@@ -10,3 +10,22 @@ var server = http.createServer(function(req, res){
 
 server.listen(3000, '127.0.0.1');
 console.log('listening to port 3000');
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+// Posluži statičke datoteke (npr. HTML)
+app.use(express.static(path.join(__dirname)));
+
+// Postavi rutu za početnu stranicu
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Pokreni server
+app.listen(PORT, () => {
+    console.log(`Server je pokrenut na http://localhost:${PORT}`);
+});
